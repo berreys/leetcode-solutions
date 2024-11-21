772aef30ac6e6f2e60e731cd7df2448153b9f434
berreys <saberrey@gmail.com>
2024-11-20 20:30:29 -0700

    first commit

---
+++ Leetcode1.py
@@ -0,0 +1,13 @@
+# LEETCODE PROBLEM 1 : TWO SUM
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        print("hello")
+
+solution = Solution()
+solution.twoSum([], 3)
