50ef18cb5dabf7bc924ed077209b86f0d59e81dd
berreys <saberrey@gmail.com>
2024-11-22 13:17:51 -0700

    leetcode1: faster solution

--- Leetcode1.py
+++ Leetcode1.py
@@ -13,5 +13,20 @@ class Solution(object):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
+    # FASTER APPROACH
+    def twoSumFaster(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in range(len(nums)):
+            if target - nums[i] in d:
+                return [d[target - nums[i]], i]
+            else:
+                d[nums[i]] = i
+
 solution = Solution()
 print(solution.twoSum([3,2,4], 6))
+print(solution.twoSumFaster([10, 20, 30, 40], 40))
