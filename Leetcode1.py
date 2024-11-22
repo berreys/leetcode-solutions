a73cc36ac91812379400c0edf5e04c55c412323d
berreys <saberrey@gmail.com>
2024-11-22 12:57:44 -0700

    leetcode1: first solution

--- Leetcode1.py
+++ Leetcode1.py
@@ -1,13 +1,17 @@
 # LEETCODE PROBLEM 1 : TWO SUM
 
 class Solution(object):
+    # NESTED FOR LOOP APPROACH
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        print("hello")
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
 solution = Solution()
-solution.twoSum([], 3)
+print(solution.twoSum([3,2,4], 6))
