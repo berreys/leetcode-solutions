cf88873f1439c96bdf8deee151396209296c5162
berreys <saberrey@gmail.com>
2024-11-23 23:35:06 -0700

    leetcode3: faster solution

--- Leetcode3.py
+++ Leetcode3.py
@@ -13,7 +13,18 @@ class Solution(object):
                 if len(set(list(s[i : j]))) == j - i:
                     longestStringLength = j - i if longestStringLength < j - i else longestStringLength
         return longestStringLength
-
+    
+    def lengthOfLongestSubstringFast(self, s):
+        longestStringLength = 0
+        uniqueChars = set()
+        left = 0
+        for right in range(len(s)):
+            while s[right] in uniqueChars:
+                uniqueChars.remove(s[left])
+                left += 1
+            uniqueChars.add(s[right])
+            longestStringLength = len(uniqueChars) if len(uniqueChars) > longestStringLength else longestStringLength
+        return longestStringLength
 
 solution = Solution()
-print(solution.lengthOfLongestSubstring('pwwkew'))
+print(solution.lengthOfLongestSubstringFast('bbbebbbb'))
