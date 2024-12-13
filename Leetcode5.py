30170287eba6d21ecdf2a481db0d702351deeda2
berreys <saberrey@gmail.com>
2024-12-13 00:31:18 -0700

    leetcode5: initialize with note

---
+++ Leetcode5.py
@@ -0,0 +1,10 @@
+# LEETCODE PROBLEM 5 : LONGEST PALINDROMIC SUBSTRING
+# https://leetcode.com/problems/longest-palindromic-substring/description/
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # TODO: complete this using sliding window
