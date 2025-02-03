3e5ae5a743aae611520d5f0980b530fbdd18b6aa
berreys <saberrey@gmail.com>
2025-02-03 16:03:27 -0700

    leetcode607: complete

--- Leetcode607.sql
+++ Leetcode607.sql
@@ -1,3 +1,12 @@
 -- LEETCODE PROBLEM 607 :  SALES PERSON
 -- https://leetcode.com/problems/sales-person/description/
 
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT s.sales_id
+    FROM SalesPerson s
+    INNER JOIN Orders o ON s.sales_id = o.sales_id
+    INNER JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
