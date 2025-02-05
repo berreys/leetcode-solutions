90b4cb706df2d99409a1c3970643564bd042fdbb
berreys <saberrey@gmail.com>
2025-02-05 09:43:30 -0700

    leetcode619: complete

--- Leetcode619.sql
+++ Leetcode619.sql
@@ -1,4 +1,13 @@
 -- LEETCODE PROBLEM 619 : BIGGEST SINGLE NUMBER
 -- https://leetcode.com/problems/biggest-single-number/description/
 
-SELECT *
+SELECT temp2.num
+FROM (SELECT NULL AS num) null_table
+LEFT JOIN (
+    SELECT * 
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+    ORDER BY num DESC
+    LIMIT 1
+) temp2 ON TRUE;
