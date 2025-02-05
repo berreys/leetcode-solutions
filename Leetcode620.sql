941c8c394b46f179bec4f9f009d63824c781e80d
berreys <saberrey@gmail.com>
2025-02-05 09:51:18 -0700

    leetcode620: complete

---
+++ Leetcode620.sql
@@ -0,0 +1,7 @@
+-- LEETCODE PROBLEM 620 : NOT BORING MOVIES
+-- https://leetcode.com/problems/not-boring-movies/description/
+
+SELECT *
+FROM Cinema
+WHERE (id % 2) = 1 AND description != "boring"
+ORDER BY rating DESC;
