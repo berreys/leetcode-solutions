d0e206bfac7f527acf0c7c25862a7a9054391f6e
berreys <saberrey@gmail.com>
2025-02-05 11:16:25 -0700

    leetcode610: complete

---
+++ Leetcode610.sql
@@ -0,0 +1,5 @@
+-- LEETCODE PROBLEM 610 : TRIANGLE JUDGEMENT
+-- https://leetcode.com/problems/triangle-judgement/description/
+
+SELECT x, y, z, IF((x + y > z) AND (x + z > y) AND (y + z > x), "Yes", "No") AS triangle
+FROM Triangle;
