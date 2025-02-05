f684f25961767c98ce89df96ad2dadb0edd070e2
berreys <saberrey@gmail.com>
2025-02-05 16:35:27 -0700

    leetcode27: initial commit

---
+++ Leetcode27.py
@@ -0,0 +1,11 @@
+# LEETCODE PROBLEM 27 : REMOVE ELEMENT
+# https://leetcode.com/problems/remove-element/description/
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
