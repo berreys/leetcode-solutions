2cedff4042060ab5938c08cabbab2a6ac9152874
berreys <saberrey@gmail.com>
2025-02-05 16:34:21 -0700

    leetcode88: complete

--- Leetcode88.py
+++ Leetcode88.py
@@ -13,21 +13,20 @@ class Solution(object):
         if n == 0:
             return
         nums1_ptr = 0
-        num_nums1_added = 0
         nums2_ptr = 0
-        while nums1_ptr < len(nums1) or nums2_ptr < n:
-            if num_nums1_added == m:
-                for i in range(nums2_ptr, n):
-                    self.insert(nums1, nums2[i], nums1_ptr)
-                    nums1_ptr += 1
+        while nums1_ptr + nums2_ptr < m + n:
+            if nums2_ptr == n:
                 return
-            if nums1[nums1_ptr] > nums2[nums2_ptr]:
-                self.insert(nums1, nums2[nums2_ptr], nums1_ptr)
-                nums1_ptr += 1
+            if nums1_ptr == m:
+                for i in range(nums1_ptr + nums2_ptr, len(nums1)):
+                    self.insert(nums1, nums2[nums2_ptr], i)
+                    nums2_ptr += 1
+                return
+            if nums1[nums1_ptr + nums2_ptr] > nums2[nums2_ptr]:
+                self.insert(nums1, nums2[nums2_ptr], nums1_ptr + nums2_ptr)
                 nums2_ptr += 1
             else:
                 nums1_ptr += 1
-                num_nums1_added += 1
 
     def insert(self, nums_list, num, index):
         for i in range(len(nums_list)-1, index, -1):
@@ -35,6 +34,12 @@ class Solution(object):
         nums_list[index] = num
 
 s = Solution()
+l1 = [2,0]
+l2 = [1]
+print(l1)
+s.merge(l1, 1, l2, 1)
+print(l1)
+
 l1 = [1,2,3,0,0,0]
 l2 = [2,5,6]
 print(l1)
