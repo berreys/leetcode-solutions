d1244a6226efc1d5ed39c4a38b6fbdce0dcb17bd
berreys <saberrey@gmail.com>
2025-02-05 20:47:59 -0700

    leetcode27: complete

--- Leetcode27.py
+++ Leetcode27.py
@@ -8,4 +8,19 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        
+        k = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] == val:
+                nums.pop(i)
+                nums.append('_')
+                k -= 1
+            else:
+                k += 1
+                i += 1
+        return k
+
+s = Solution()
+l = [0,1,2,2,3,0,4,2]
+s.removeElement(l, 2)
+print(l)
