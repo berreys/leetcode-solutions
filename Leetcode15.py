ff5a699c231bfebc4d678d0d4b7bcf1db9238132
berreys <saberrey@gmail.com>
2025-02-06 11:06:01 -0700

    leetcode15: slow algorithm complete

--- Leetcode15.py
+++ Leetcode15.py
@@ -7,4 +7,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        threeSums = set()
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        temp = sorted([nums[i], nums[j], nums[k]])
+                        threeSums.add((temp[0], temp[1], temp[2]))
+        result = []
+        for tuple in threeSums:
+            result.append(list(tuple))
+        return result
+    
+s = Solution()
+print(s.threeSum([-1,0,1,2,-1,-4]))
