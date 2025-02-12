38314144b896b6d3b20f0da9f2a34543f4865a52
berreys <saberrey@gmail.com>
2025-02-12 15:05:32 -0700

    leetcode238: complete

--- Leetcode238.py
+++ Leetcode238.py
@@ -13,6 +13,28 @@ class Solution(object):
                 if j != i:
                     answer[i] *= nums[j]
         return answer
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer = [1 for _ in nums]
+        pre = [i for i in nums]
+        post = [i for i in nums]
+        for i in range(1, len(nums)):
+            pre[i] *= pre[i - 1]
+        for i in range(len(nums) - 2, -1, -1):
+            post[i] *= post[i + 1]
+        for i in range(len(nums)):
+            if i == 0:
+                answer[i] = post[i + 1]
+            elif i == len(nums) - 1:
+                answer[i] = pre[i - 1]
+            else:
+                answer[i] = post[i + 1] * pre[i - 1]
+        return answer
+
+
 
 s = Solution()
 print(s.productExceptSelf([1,2,3,4]))
