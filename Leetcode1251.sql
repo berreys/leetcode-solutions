5b4ab8b677f65c0f62396e58139b9e99b2ae4c0c
berreys <saberrey@gmail.com>
2025-04-29 10:54:05 -0600

    leetcode1251: complete

---
+++ Leetcode1251.sql
@@ -0,0 +1,8 @@
+-- LEETCODE PROBLEM 1251 : AVERAGE SELLING PRICE
+-- https://leetcode.com/problems/average-selling-price/description/
+
+SELECT p.product_id, IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON u.product_id = p.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
