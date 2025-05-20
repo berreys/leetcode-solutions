4bb67722e263746e2da2eb54735b00f2efd0aeae
berreys <saberrey@gmail.com>
2025-05-20 16:06:46 -0600

    leetcode15: complete

--- Leetcode15.py
+++ Leetcode15.py
@@ -1,74 +1,64 @@
 # LEETCODE PROBLEM 15 : 3SUM
 # https://leetcode.com/problems/3sum/description/
 
+cache = {}
+cache2 = {}
 class Solution(object):
-    def threeSumSlow(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        threeSums = set()
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                for k in range(j+1, len(nums)):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        temp = sorted([nums[i], nums[j], nums[k]])
-                        threeSums.add((temp[0], temp[1], temp[2]))
-        result = []
-        for tuple in threeSums:
-            result.append(list(tuple))
-        return result
-    def binarySearchForNumber(self, nums, target): # return index
-        if len(nums) == 0 or len(nums) == 1 and nums[0] != target:
-            return -1
-        if len(nums) == 1 and nums[0] == target:
-            return 0
-        middle = nums[int(len(nums) / 2)]
-        if target == middle:
-            return int(len(nums) / 2)
-        if target < middle:
-            return self.binarySearchForNumber(nums[: int(len(nums) / 2)], target)
-        if target > middle:
-            index = self.binarySearchForNumber(nums[int(len(nums) / 2):], target)
-            if index == -1:
-                return index
-            return int(len(nums) / 2) + index
-    def threeSum(self, nums):
-        nums = list(set(nums))
-        nums.sort()
-        results = []
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                compliment_index = self.binarySearchForNumber(nums, 0 - nums[i] - nums[j])
-                if compliment_index != -1 and compliment_index != i != j:
-                    results.append([nums[i], nums[j], nums[compliment_index]])
-        return results
     def twoSumHelper(self, nums, target):
+        if str(nums) + " " + str(target) in cache:
+            return cache[str(nums) + " " + str(target)]
+        results = []
         d = {}
         for i in range(len(nums)):
             if target - nums[i] in d:
-                return [d[target - nums[i]], i]
+                results.append([d[target - nums[i]], i])
             else:
                 d[nums[i]] = i
+        cache[str(nums) + " " + str(target)] = results
+        return results
     def twoSum(self, nums, target):
+        if str(nums) + " " + str(target) in cache2:
+            return cache2[str(nums) + " " + str(target)]
         temp = self.twoSumHelper(nums, target)
-        if temp:
-            return [nums[temp[0]], nums[temp[1]]]
+        results = []
+        for i in range(len(temp)):
+            results.append([nums[temp[i][0]], nums[temp[i][1]]])
+        cache2[str(nums) + " " + str(target)] = results
+        return results
     def threeSumFaster(self, nums):
+        freqs = {}
+        for i in nums:
+            if i in freqs:
+                freqs[i] += 1
+            else:
+                freqs[i] = 1
+        nums = []
+        for i in freqs:
+            if i == 0 and freqs[i] > 2:
+                nums.append(i)
+                nums.append(i)
+                nums.append(i)
+            if freqs[i] > 1:
+                nums.append(i)
+                nums.append(i)
+            else:
+                nums.append(i)
         results = set()
         for i in range(len(nums)):
             temp = self.twoSum(nums[:i] + nums[i+1:], -1 * nums[i])
-            if temp:
-                temp.append(nums[i])
-                results.add(tuple(sorted(temp)))
+            for j in range(len(temp)):
+                results.add(tuple(sorted(temp[j] + [nums[i]])))
         return [list(i) for i in results]
             
     
 
     
 s = Solution()
-print(s.threeSumFaster([-1,0,1,2,-1,-4]))
-print(s.threeSumFaster([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
+# print(s.threeSumFaster([-1,0,1,2,-1,-4]))
+# print(s.threeSumFaster([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
+print(s.threeSumFaster([0,0,1,1,-1,-1]))
+print(s.threeSumFaster([0,0,0]))
+
 # print(s.binarySearchForNumber([1,2,3,4,5], 1))
 # print(s.binarySearchForNumber([1,2,3,4,5], 2))
 # print(s.binarySearchForNumber([1,2,3,4,5], 3))
